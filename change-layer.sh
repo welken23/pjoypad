211bad2cce72b1c597ca0c0c24377025d00c4c9c
welken23 <alexsharikov23@gmail.com>
2024-07-22 00:11:02 +0300

    Update change-layer.sh, added gdbus call to close notification

--- change-layer.sh
+++ change-layer.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-notify-send "$1 $2 $3"
+# notification display time in milliseconds
+time=1800
+# notification id
+id=10000
+# close notification with id
+gdbus call --session --dest org.freedesktop.Notifications --object-path /org/freedesktop/Notifications --method org.freedesktop.Notifications.CloseNotification $id > /dev/null
+# show notification with id
+notify-send "$1 $2 $3" -t $time -r $id
 $1 $2 $3 &
