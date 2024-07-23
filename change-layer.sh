6a445696e96ba6e3ee4414f897b7bb7d458ef04f
welken23 <alexsharikov23@gmail.com>
2024-07-23 20:23:30 +0300

    Update change-layer.sh: removed notifications. The structure of layers has been changed. Files layer0.yml, layer1.yml, layer2.yml updated in accordance with the new structure

--- change-layer.sh
+++ change-layer.sh
@@ -6,6 +6,4 @@ time=1800
 id=10000
 # close notification with id
 gdbus call --session --dest org.freedesktop.Notifications --object-path /org/freedesktop/Notifications --method org.freedesktop.Notifications.CloseNotification $id > /dev/null
-# show notification with id
-notify-send "$1 $2 $3" -t $time -r $id
 $1 $2 $3 &
